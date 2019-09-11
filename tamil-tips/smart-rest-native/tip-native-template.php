21b47cdd7de50c61216f30133b0b4cd342691fd6
SmartDroidies <smartdroidies@gmail.com>
2019-09-11 05:53:30 -0400

    Tips by ids

--- tamil-tips/smart-rest-native/tip-native-template.php
+++ tamil-tips/smart-rest-native/tip-native-template.php
@@ -4,13 +4,21 @@ smartrest_do_json_native_tip();
 
 function smartrest_do_json_native_tip() {
 	
-     $tipId = $_GET['id'];
+    $tipId = $_GET['id'];
+    $tipIds = $_GET['ids'];
 
-	// WP_Query arguments
-    $args = array ( 'p' => $tipId, 
-				   'post_type' => 'any',
-				    'post_status' => array('publish', 'future'));	
-	
+	if(isset($tipIds)) {
+		// WP_Query arguments
+	    $args = array ( 'post__in' => explode(",", $tipIds), 
+					   'post_type' => 'any',
+					    'post_status' => array('publish', 'future'));	
+	} else {
+		// WP_Query arguments
+	    $args = array ( 'p' => $tipId, 
+					   'post_type' => 'any',
+					    'post_status' => array('publish', 'future'));	
+	}
+
 	// The Query
 	$query = new WP_Query( $args );
 	while($query->have_posts()) {
@@ -31,7 +39,7 @@ function smartrest_do_json_native_tip() {
 
 		
 	   $accesstime = get_post_time();
-	   $result = array("kurippuId"=>get_the_ID(), "title"=>get_the_title(), "postDate"=>get_post_time(), "updatedDate"=>get_the_modified_time('U'), "category"=> $arrCatId[0], "status" => get_post_status(), "image" => $postimage, "content" => get_the_content() );
+	   $result[] = array("kurippuId"=>get_the_ID(), "title"=>get_the_title(), "postDate"=>get_post_time(), "updatedDate"=>get_the_modified_time('U'), "category"=> $arrCatId[0], "status" => get_post_status(), "image" => $postimage, "content" => get_the_content() );
 	}
 	write_header();
     echo json_encode($result); 
